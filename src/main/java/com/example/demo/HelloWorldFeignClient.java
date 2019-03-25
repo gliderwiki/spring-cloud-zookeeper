2f00b6805bf82c3d0932ca1500f15c649a7f2aee
edell.lee <edell.lee@kakaocorp.com>
2019-03-25 15:47:27 +0900

    spring boot zookeeper discovery example

---
+++ src/main/java/com/example/demo/HelloWorldFeignClient.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@FeignClient(name = "zookeeper-provider")
+public interface HelloWorldFeignClient {
+	
+	@RequestMapping(path = "/helloworld", method = RequestMethod.GET)
+    @ResponseBody
+	String helloWorld();
+	
+	@RequestMapping(path = "/name", method = RequestMethod.GET)
+    @ResponseBody
+	String name();
+	
+	@RequestMapping(path = "/product", method = RequestMethod.GET)
+    @ResponseBody
+	String product();
+}
